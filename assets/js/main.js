68a12201945b4bba06bd05411dd24ff6d04914ce
Manuel Nieto <nietosoftwaredeveloper@outlook.com>
2024-03-25 20:13:09 -0500

    ok

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
-function scrollUp(){
+function scrollUp() {
     const scrollUp = document.getElementById('scroll-up');
     // When the scroll is higher than 350 viewport height, add the show-scroll class to the a tag with the scroll-top class
-    if(this.scrollY >= 350) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
+    if (this.scrollY >= 350) scrollUp.classList.add('show-scroll'); else scrollUp.classList.remove('show-scroll')
 }
 window.addEventListener('scroll', scrollUp)
 
@@ -15,10 +15,10 @@ const sr = ScrollReveal({
 const d = new Date();
 document.getElementById("date").innerHTML = d;
 
-sr.reveal(`.welcome`, {delay: 100}); 
-sr.reveal(`.card`, {delay: 150}); 
-sr.reveal(`.footer__logo`, {delay: 200}); 
-sr.reveal(`.footer__content`, {interval: 250});
-sr.reveal(`.footer__copy`), {interval: 300};
-sr.reveal(`footer__dev-link`, {interval: 300});
-sr.reveal(`date`, {interval:350}) 
+sr.reveal(`.welcome`, { delay: 100 });
+sr.reveal(`.card`, { delay: 150 });
+sr.reveal(`.footer__logo`, { delay: 200 });
+sr.reveal(`.footer__content`, { interval: 250 });
+sr.reveal(`.footer__copy`), { interval: 300 };
+sr.reveal(`footer__dev-link`, { interval: 300 });
+sr.reveal(`date`, { interval: 350 }) 
